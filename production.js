a0f4e37c5c815ca2e825e6dd24558bfbb0befcde
dykoffi <dykoffi@gmail.com>
2021-01-10 07:05:58 +0000

    ok

---
+++ production.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http')
+const favicon = require('express-favicon');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3620;
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, 'build', 'index.html')); });
+http.createServer(app).listen(port);
+console.log(`Worker ${process.pid} is running`);
