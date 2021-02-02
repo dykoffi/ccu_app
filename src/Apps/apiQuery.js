ff530fe59810f2b15e51ccf66c0b3df0cdcf7f24
dykoffi <dykoffi@gmail.com>
2021-02-02 04:20:36 +0000

    change api port to 37000

--- src/Apps/apiQuery.js
+++ src/Apps/apiQuery.js
@@ -4,7 +4,7 @@ import { setSESSIONID } from './redux'
 const host = "160.120.185.111" //window.location.hostname;
 const crypto = require('crypto')
 const cookies = new Cookies()
-export const header = { url: `http://${host}:4001` };
+export const header = { url: `http://${host}:37000` };
 
 if (!cookies.get("sessionID")) {
     let sessionid = crypto.randomBytes(64).toString('base64') + (new Date()).getTime()
